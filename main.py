cce7fbac71d25ff6f6bd2ce13b9e82bdd89b9473
Jaya Kandpal <jaya.kandpal99@gmail.com>
2023-10-26 13:38:40 +0530

    Refined main.py- added more comments, commented testing code lines and added a line to reveal the ans in case the player loses.

--- main.py
+++ main.py
@@ -1,60 +1,74 @@
+import random
 import hangman_words
 import hangman_art
-import random
 
 # Updating the word list to use the 'word_list' from hangman_words.py
+# Choosing random word from list
+# Deleted this - word_list = ["blobfish", "platypus", "shoebill", "baboon"]
 chosen_word = random.choice(hangman_words.word_list)
-word_length = len(chosen_word)
 
+# Creating a variable that indicates that the game has not ended
 end_of_game = False
+
+# Create a variable called 'lives' to keep track of the number of lives left.
 lives = 6
 
-# Importing the logo from hangman_art.py and print it at the start of the game.
+# Importing the logo from hangman_art.py to print it at the start of the game.
 print(hangman_art.logo)
 
-# Testing code
-print(f'Pssst, the answer is {chosen_word}.')
+# For code testing
+# print(f"Pssst, the solution is {chosen_word}.")
 
-# Creating blanks
+# Created an empty List called display.
+# For each letter in the chosen_word, add a "_" to 'display'.
 display = []
 guessed = []
-for _ in range(word_length):
-    display += "_"
+for letter in chosen_word:
+    display.append('_')
 
 while not end_of_game:
+    # Asking the user for a letter
     guess = input("Guess a letter: ").lower()
 
-    # Checking guessed letter
-    for position in range(word_length):
-        letter = chosen_word[position]
-        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+    # Looping through each position in the chosen_word
+    # If the letter at that position matches 'guess' then reveal that letter in the display at that position.
+    position = -1
+    for letter in chosen_word:
+        position += 1
         if letter == guess:
             display[position] = letter
+    # OR
+    # for position in range(len(chosen_word)):
+    #   letter = chosen_word[position]
 
-    # Checking if user is wrong.
-    # If the letter is not in the chosen_word, print out the letter and let the player know it's not in the word.
+    # If guess is not a letter in the chosen_word,then reduce 'lives' by 1.
+    # If lives goes down to 0 then the game should stop, and it should print "You lose."
+    # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
     if guess not in chosen_word:
         print(f"{guess} is not in the word.")
         if guess not in guessed:
             lives -= 1
             if lives == 0:
+                # To end the while loop setting the end_of_game variable to true
                 end_of_game = True
+                print(f"This was the word: {chosen_word}")
                 print("You lose")
 
     # If the user has entered a letter they've already guessed, print the letter and let them know.
+
     if guess not in guessed:
         guessed.append(guess)
-        print(guessed)
+        # print(guessed)
     else:
         print(f"You have already guessed letter {guess}.")
 
-    # Joining all the elements in the list and turning it into a String.
+    # Join all the elements in the list and turn it into a String. So it comes as a string is display.
     print(f"{' '.join(display)}")
 
-    # Checking if user has got all letters.
+    # Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
         print("You win.")
 
-    # Importing the stages from hangman_art.py.
+# Importing the stages from hangman_art.py
     print(hangman_art.stages[lives])
